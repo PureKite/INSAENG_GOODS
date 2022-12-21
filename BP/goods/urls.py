9f41a5f18e7276db657b2f20ab0c82de9691303b
a025160 <a025160@aivle.kt.co.kr>
2022-12-21 15:40:08 +0900

    1221_1

--- BP/goods/urls.py
+++ BP/goods/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from goods.views import makegoods
+from goods.views import makegoods,downloadFile
 
 
 app_name = 'goods'
 
 urlpatterns = [
     path('', makegoods, name='makegoods'),
+    path("download/", downloadFile, name="downloadFile"),
 ]
