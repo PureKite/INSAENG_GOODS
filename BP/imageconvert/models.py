cb3070556e6bda3daf00c07c7de5688d27fb2efe
phj799 <phj799@naver.com>
2022-12-29 12:37:20 +0900

    model적용

--- BP/imageconvert/models.py
+++ BP/imageconvert/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from io import *
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
+
+class Images(models.Model):
+    user_id = models.AutoField(auto_created = True, primary_key = True)
+    raw_img = models.ImageField(upload_to='raw_img',null=False)
+    cvt_img = models.ImageField(upload_to='cvt_img',null=True)
