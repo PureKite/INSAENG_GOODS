c470707ebf32d8d84f6a4f16fd26c28f660a01a5
phj799 <phj799@naver.com>
2022-12-29 13:15:15 +0900

    media 폴더 경로 수정

--- BP/imageconvert/models.py
+++ BP/imageconvert/models.py
@@ -6,4 +6,4 @@ import sys
 class Images(models.Model):
     user_id = models.AutoField(auto_created = True, primary_key = True)
     raw_img = models.ImageField(upload_to='raw_img',null=False)
-    cvt_img = models.ImageField(upload_to='cvt_img',null=True)
+    cvt_img = models.ImageField(upload_to='cvt_img',null=False)
