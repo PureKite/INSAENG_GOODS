ef8f0b81a7a7dea91f232b492e5c24739a0b0126
a025160 <a025160@aivle.kt.co.kr>
2023-01-03 16:09:37 +0900

    bg.py

--- BP/rembg/rembg/__init__.py
+++ BP/rembg/rembg/__init__.py
@@ -3,4 +3,6 @@ from . import _version
 __version__ = _version.get_versions()["version"]
 
 from .bg import remove
+from .bg import remove1
+from .bg import remove2
 from .session_factory import new_session
